e33c38113372024542e23f073e392ddb58f6c01e
Samuel Adriano Tomimatu <samuckqa@hotmail.com>
2021-06-26 23:42:20 -0300

    criar comidinha

--- SnakeGame/assets/js/script.js
+++ SnakeGame/assets/js/script.js
@@ -12,6 +12,11 @@ snake[0] = {
 
 let direction = 'right'
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1 ) * box, 
+    y:Math.floor(Math.random() * 15 + 1) * box
+}
+
 function createBg() {
     context.fillStyle = 'lightgreen'
     context.fillRect(0, 0, 16 * box, 16 * box)
@@ -23,6 +28,11 @@ function createSnake() {
         context.fillStyle = 'green'
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+function drawFood(){
+    context.fillStyle = 'red'
+    context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener('keydown', update) //keydown = evento de click;
@@ -52,6 +62,7 @@ function inicializeGame() {
 
     createBg()
     createSnake()
+    drawFood()
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
