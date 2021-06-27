0cc2d476f63b0d33da87826565994282a9afda88
Samuel Adriano Tomimatu <samuckqa@hotmail.com>
2021-06-26 23:34:06 -0300

    definir direções, estilização  e impedir que a cobrinha suma

--- SnakeGame/assets/js/script.js
+++ SnakeGame/assets/js/script.js
@@ -25,8 +25,32 @@ function createSnake() {
     }
 }
 
+document.addEventListener('keydown', update) //keydown = evento de click;
+
+function update(event) {
+    if (event.keyCode == 37 && direction != 'right') {
+        direction = 'left'
+    } else if (event.keyCode == 38 && direction != 'down') {
+        direction = 'up'
+    } else if (event.keyCode == 39 && direction != 'left') {
+        direction = 'right'
+    } else if (event.keyCode == 40 && direction != 'up') {
+        direction = 'down'
+    }
+}
+
 function inicializeGame() {
-    createBg();
+    if (snake[0].x > 15 * box && direction == 'right') {
+        snake[0].x = 0
+    } else if (snake[0].x < 0 && direction == 'left') {
+        snake[0].x = 16 * box;
+    } else if (snake[0].y > 15 * box && direction == 'down') {
+        snake[0].y = 0
+    } else if (snake[0].y < 0 && direction == 'up') {
+        snake[0].y = 16 * box
+    }
+
+    createBg()
     createSnake()
 
     let snakeX = snake[0].x;
@@ -34,14 +58,11 @@ function inicializeGame() {
 
     if (direction == 'right') {
         snakeX += box;
-    }
-    if (direction == 'left') {
+    } else if (direction == 'left') {
         snakeX -= box;
-    }
-    if (direction == 'up') {
+    } else if (direction == 'up') {
         snakeY -= box;
-    }
-    if (direction == 'down') {
+    } else if (direction == 'down') {
         snakeY += box;
     }
 
